f77aa488ee11378e4e1fb0fe636fbe2964b051b4
Slasherss <jszumotalski@gmail.com>
2019-06-02 12:34:43 +0200

    Added function 1

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ const path = require('path');
 const port = 3000
 
 app.get('/', function(req, res) {res.sendFile(path.join(__dirname + "/index.html"))})
+app.get('/1', function(req, res) {
+  res.sendFile(path.join(__dirname + "/1/index.html"))
+  console.log("Clicked fun1")
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

72392fcc3ec94f1432f555f28035508f7bc3301e
Slasherss <jszumotalski@gmail.com>
2019-06-02 15:04:11 +0200

    Edited styles

--- main.js
+++ main.js
@@ -10,7 +10,31 @@ obs.connect({address: 'localhost:4444'});
 app.get('/', function(req, res) {res.sendFile(path.join(__dirname + "/index.html"))})
 app.get('/1', function(req, res) {
   res.sendFile(path.join(__dirname + "/1/index.html"))
-  console.log("Clicked fun1")
+  console.log("Clicked Function 1")
+  obs.send('GetMute', {'source': 'Mic'}).then(function(name) {
+    obs.send('SetMute', {
+      'source': 'Mic',
+      "mute": !name.muted
+    });
+  })
 })
+app.get('/2', function(req, res) {
+  res.sendFile(path.join(__dirname + "/2/index.html"))
+  console.log("Clicked Function 2")
+  obs.send('SetSourceRender', {
+    "source": "Webcam",
+    "render": true
+  })
+})
+app.get('/3', function(req, res) {
+  res.sendFile(path.join(__dirname + "/3/index.html"))
+  console.log("Clicked Function 3")
+  obs.send('SetSourceRender', {
+    "source": "Webcam",
+    "render": false
+  })
+})
+
+process.on('exit', () => {obs.disconnect()})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
